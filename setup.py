016bd48e560be9fb08db8059c64d0ef39603bd46
RaikoPipe <eclipsedust@gmail.com>
2024-08-15 13:12:19 +0200

    added setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='sb3_extensions',
     version='1',
-    packages=['stable-baselines3'],
+    packages=find_packages(),
     url='https://github.com/RaikoPipe/sb3_extensions',
+    install_requires=["stable-baselines3"],
     license='',
     author='Richard Reider',
     author_email='richard.reider@ovgu.de',
